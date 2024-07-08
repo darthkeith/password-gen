76cd7d425bcbe017ec6b9137f83afc6e041c20e4
Keith Kaplan <keithkaplan0@gmail.com>
2024-07-08 16:54:21 -0700

    Add default password length

--- passwordgen.py
+++ passwordgen.py
@@ -4,7 +4,10 @@
 Prints a randomly generated password of a given length to the console.
 
 Usage:
-    ./passwordgen.py <password length>
+    ./passwordgen.py [<password length>]
+
+    <password length> :
+        A positive integer. If omitted, a default length is used.
 
 Notes
 -----
@@ -14,10 +17,12 @@ Uses true random number generation for security.
 from sys import argv
 from secrets import choice
 
+DEFAULT_LENGTH = 20
+
 # All printable ASCII characters excluding <space>
 CHARS = ''.join(chr(i) for i in range(33, 127))
 
-length = int(argv[1])
+length = DEFAULT_LENGTH if len(argv) <= 1 else int(argv[1])
 password = ''.join(choice(CHARS) for _ in range(length))
 print(password)
 
